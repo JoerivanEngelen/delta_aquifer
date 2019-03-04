bd19a321b789a8f0153cea12a871aeebeca51ab4
Joeri van Engelen <Joeri.vanEngelen@deltares.nl>
2019-03-04 19:17:16 +0100

    Clay layers tops and bots, good to go

--- geometry.py
+++ geometry.py
@@ -13,8 +13,12 @@ import os
 from mpl_toolkits.mplot3d import Axes3D #We have to import this to allow 3d plotting, otherwise projection = "3d" not recognized
 
 #%%Helper functions
-def _griddata(x, y, val, targgrid, method="linear"):
-    return(np.reshape(interpolate.griddata((x, y), val, tuple([i.flatten() for i in targgrid]), method=method), targgrid[0].shape))
+def _griddata(ip, val, targgrid):
+    if val.ndim == 1:
+        ip.values = np.expand_dims(val, 1)
+    else:
+        ip.values = val
+    return(np.reshape(ip(tuple([i.flatten() for i in targgrid])), targgrid[0].shape))
 
 def _clip_f(arr, sel, a_min=None, a_max=None):
     """Wrapper for clipping with fancy indexing"""
@@ -27,8 +31,8 @@ def _clip_f(arr, sel, a_min=None, a_max=None):
     arr[:, sel] = np.clip(arr[:, sel], check(a_min, sel), check(a_max, sel))
     return(arr)
 
-def _ellipse(phi, D, x):
-    return(np.multiply.outer(D/phi,np.sqrt(phi**2-x**2)))
+def _ellipse(phi, D, phis):
+    return(np.multiply.outer(D/phi,np.sqrt(phi**2-phis**2)))
 
 def _cart2pol(x, y):
     rho = np.sqrt(x**2 + y**2)
@@ -98,24 +102,32 @@ def create_top_bot(phi, d2):
         arr[nan_idx]=np.nan
     return(d3, nan_idx, phis)
 
-def create_confining_layer(clay_conf, d3, d2, phis, L, a):
+def create_confining_layer(clay_conf, d3, d2, phis, L, a, frac):
     d3_clay = {}
     idx_onshore = d2["rho"] < L*a
     x_ons, y_ons = _pol2cart(*np.meshgrid(d2["rho"][idx_onshore], phis))
     
-    rech=L * a * (1-clay_conf)
-    rho_clay, phi_clay = _cart2pol(x_ons[0][-1] - rech, y_ons[0][-1])
-    
+    x_offset=L * a * (1-clay_conf)
+    rho_clay, phi_clay = _cart2pol(x_ons[0][-1] - x_offset, y_ons[0][-1])
     rhos_clay = np.linspace(0, rho_clay, num=ncell)
     phis_clay = np.linspace(-phi_clay, phi_clay, num=ncell-20)
+    
+    topf = interpolate.interp1d(d2["rho"], d2["top"])
+    botf = interpolate.interp1d(d2["rho"], d2["bot"])
+    
+    depth_conf = topf(rhos_clay+x_offset) + botf(rhos_clay+x_offset) * frac
+    d3_clay["bots"] = -_ellipse(phi_clay, depth_conf, phis_clay).T
+    d3_clay["tops"] = np.zeros(d3_clay["bots"].shape) + topf(rhos_clay+x_offset) #Hack to get 2D tops of clay layer
+    
     d3_clay["x"], d3_clay["y"] = _pol2cart(*np.meshgrid(rhos_clay, phis_clay))
-    d3_clay["x"] += rech
+    d3_clay["x"] += x_offset
     
     #deactivate clay_layer past coastline
     nan_clay = np.greater(d3_clay["x"].T, np.nanmax(x_ons, axis = 1)).T
     for key, arr in d3_clay.items():
-        d3_clay[key] = arr[~nan_clay]
-    return(d3_clay)
+        arr[nan_clay] = np.nan
+#        d3_clay[key] = arr[~nan_clay]
+    return(d3_clay, nan_clay)
 
 def get_targgrid(dx, dy, L, phi_max):
     x_edge, y_edge = _pol2cart(L, phi_max)
@@ -125,6 +137,14 @@ def get_targgrid(dx, dy, L, phi_max):
     y_out = np.arange(-y_max, y_max+1, dy)
     return(np.meshgrid(x_out, y_out))
 
+def create_clayer(frac, bot, d3, phis, phi, a_off = 0.):
+    
+#    depth_clay = d2["top"] + (d2["top"]+d2["bot"])*(frac)
+    depth_clay = a_off + bot*frac
+    clayer = (_ellipse(phi/2, depth_clay, phis).T + depth_clay)/2.
+    in_prism=(clayer<d3["tops"]) & (clayer>d3["bots"])
+    clayer[~in_prism] = np.nan
+    return(clayer)
 
 #%%Path management
 figfol=r"c:\Users\engelen\OneDrive - Stichting Deltares\PhD\Synth_Delta\Modelinput\Figures"
@@ -157,9 +177,10 @@ phi = np.linspace(0.125, 0.5, num=lev) * np.pi
 clay_conf = np.linspace(0.2, 1., num=lev)
 
 n_clay = np.linspace(0, 3, num=lev, dtype=int)
-clay_frac = 0.5 #FIXED FOR NOW ELSE np.linspace(0.1, 0.6, num=4)
+SM = 0.3 #FIXED FOR NOW ELSE np.linspace(0.1, 0.6, num=4)
 
 #%%For testing
+#Domain geometry
 a = a[1]
 b = b[1]
 D = D[2]
@@ -168,8 +189,8 @@ alpha = alpha[2]
 beta = beta[2]
 phi = phi[2]
 
+#Internal geometry
 clay_conf = clay_conf[2]
-
 n_clay = n_clay[3]
 
 #%%Process central crosssection
@@ -196,54 +217,93 @@ plt.savefig(os.path.join(figfol, "3D_top_bot.png"))
 plt.close()
 
 #%%Create confining layer
-d3_conf = create_confining_layer(clay_conf, d3, d2, phis, L, a)
+frac_sand = (1-SM) / (n_clay + 1)
+frac_clay = SM/(n_clay+1)
+
+d3_conf,nan_conf = create_confining_layer(clay_conf, d3, d2, phis, L, a, frac_clay)
 
 #%%Create clay layers
-d3_clay = {}
+fracs = np.cumsum([frac_sand] + [frac_clay, frac_sand]*n_clay)
 
-clay_fracs = np.arange(1, n_clay+1)/(n_clay+1)
+for i in range(n_clay):
+    d3["ct%d"%i] = create_clayer(fracs[0::2][i], d2["bot"], d3, phis, phi)
+    d3["cb%d"%i] = create_clayer(fracs[1::2][i], d2["bot"], d3, phis, phi)
 
-for i, frac in enumerate(clay_fracs):
-    #This fractioning does not work yet...
-    depth_clay_top = d2["top"] + (d2["top"]+d2["bot"])*(frac - clay_frac/(n_clay*2))
-    depth_clay_bot = d2["top"] + (d2["top"]+d2["bot"])*(frac + clay_frac/(n_clay*2))
-    d3_clay["ct%d"%i] = (_ellipse(phi/2, depth_clay_top, phis).T + depth_clay_top)/2.
-    d3_clay["cb%d"%i] = (_ellipse(phi/2, depth_clay_bot, phis).T + depth_clay_bot)/2.
-    in_prism=(d3_clay["ct%d"%i]<d3["tops"]) & (d3_clay["ct%d"%i]>d3["bots"])
-    d3_clay["ct%d"%i][~in_prism] = np.nan
-    in_prism=(d3_clay["cb%d"%i]<d3["tops"]) & (d3_clay["cb%d"%i]>d3["bots"])
-    d3_clay["cb%d"%i][~in_prism] = np.nan
+##Plot
+slcs = [np.s_[:, 50],np.s_[90, :]]
+dims = ["y", "x"]
 
-plt.plot(d3["bots"][:, -50])
-plt.plot(d3["tops"][:, -50])
-for key, clayer in d3_clay.items():
-    plt.plot(clayer[:, -50], label = key)
+fig, axes = plt.subplots(ncols=2, figsize=(8, 4))
 
+for i, slc in enumerate(slcs):
+    xtop = xbot = d3[dims[i]][slc]
+    axes[i].plot(xbot, d3["bots"][slc], label = "bot")
+    axes[i].plot(xtop, d3["tops"][slc], label = "top")
+    
+    if dims[i] == "y":
+        xval = d3["x"][slc][90]
+        idx = np.searchsorted(d3_conf["x"][90, :], xval)
+        idx = np.s_[:, idx]
+    else:
+        idx = slc
+        
+    xconf = d3_conf[dims[i]][idx]
+
+    axes[i].fill(np.append(xconf[::-1], xconf), np.append(d3_conf["tops"][idx][::-1], d3_conf["bots"][idx]), label="conf")
+    
+    for j in range(n_clay):
+        xtop = xbot = d3[dims[i]][slc]
+        top = d3["ct%d"%j][slc]
+        bot = d3["cb%d"%j][slc]
+        
+        xtop = xtop[~np.isnan(top)]
+        xbot = xbot[~np.isnan(bot)]
+        
+        axes[i].fill(np.append(xtop[::-1], xbot), np.append(top[~np.isnan(top)][::-1], bot[~np.isnan(bot)]), label = "c%d"%j)
+    
 plt.legend()
+plt.tight_layout()
+plt.savefig(os.path.join(figfol, "clayers.png"))
+plt.close()
+
+
 
 ##%%Convert to Cartesian regular grid
+#d3_grid = {}
+#
 #dx, dy = 1000, 1000
 #
-#targgrid = get_targgrid(dx, dy, L, phi/2)
+#d3_grid["x"], d3_grid["y"] = get_targgrid(dx, dy, L, phi/2)
 #
 ##Remove nans,which broadcasts to 1 dimensional array.
 #for key, arr in d3.items():
 #    d3[key] = arr[~nan_idx]
 #
-#topgrid  = _griddata(d3["x"], d3["y"], d3["tops"], targgrid)
-#botgrid  = _griddata(d3["x"], d3["y"], d3["bots"], targgrid)
-#claygrid = _griddata(d3_conf["x"], d3_conf["y"], np.ones(d3_conf["y"].shape), targgrid, method="linear")
+##Explicitly create NDINterpolator once so QHULL has to be called only once, _griddata then just overrides the values.
+#ip = interpolate.LinearNDInterpolator(np.vstack((d3["x"],d3["y"])).T, d3["tops"])
+#vrbls = [i for i in d3.keys() if not i in ["x", "y"]]
+#for vrbl in vrbls:
+#    d3_grid[vrbl] = _griddata(ip, d3[vrbl], (d3_grid["x"], d3_grid["y"]))
 #
-#plt.imshow(topgrid)
+#for key, arr in d3_conf.items():
+#    d3_conf[key] = arr[~nan_conf]
+#
+##Now for confining layer
+#ip = interpolate.LinearNDInterpolator(np.vstack((d3_conf["x"],d3_conf["y"])).T, d3_conf["tops"])
+#vrbls = [i for i in d3_conf.keys() if not i in ["x", "y"]]
+#for vrbl in vrbls:
+#    d3_grid[r"conf_"+vrbl] = _griddata(ip, d3_conf[vrbl], (d3_grid["x"], d3_grid["y"]))
+#
+#plt.imshow(d3_grid["tops"])
 #plt.savefig(os.path.join(figfol, "top_grid.png"))
 #plt.close()
 #
-#plt.imshow(botgrid)
+#plt.imshow(d3_grid["bots"])
 #plt.savefig(os.path.join(figfol, "bot_grid.png"))
 #plt.close()
 #
 ##%%Create topsystem
-#topsys = np.nan_to_num(topgrid/topgrid) + np.nan_to_num(claygrid) + (topgrid >= 0)
+#topsys = np.nan_to_num(d3_grid["tops"]/d3_grid["tops"]) + np.nan_to_num(d3_grid["conf_tops"]/d3_grid["conf_tops"]) + (d3_grid["tops"] >= 0)
 #
 #plt.imshow(topsys)
 #plt.savefig(os.path.join(figfol, "topsystem_grid.png"))
