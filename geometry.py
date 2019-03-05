b8a51dc7718a3424a0d7fdb1cdb25177356475a5
Joeri van Engelen <Joeri.vanEngelen@deltares.nl>
2019-03-05 11:25:10 +0100

    Changed d3 in d2 and d2 in d1, to better name dimensions

--- geometry.py
+++ geometry.py
@@ -60,74 +60,74 @@ def _calc_top(a, alpha, b, beta, gamma, L,  x):
     return(z, c_cells)
 
 def _calc_bot(apex, a, D, dD, L, x): 
-    D2 = apex - D*dD
-    slope = (D2 + D)/(L*a)
-    return(D2 - x*slope)
+    d1 = apex - D*dD
+    slope = (d1 + D)/(L*a)
+    return(d1 - x*slope)
 
 #%%Main functions
 def create_crosssection(a, alpha, b, beta, gamma, dD, D, L):
-    d2 = {}
+    d1 = {}
 
     gamma *= D/1000
     
-    d2["rho"] = np.linspace(0, L, num=ncell)
-    d2["top"], d2["slope"] = _calc_top(a, alpha, b, beta, gamma, L, d2["rho"])
-    d2["bot"] = _calc_bot(d2["top"][0], a, D, dD, L,  d2["rho"])
+    d1["rho"] = np.linspace(0, L, num=ncell)
+    d1["top"], d1["slope"] = _calc_top(a, alpha, b, beta, gamma, L, d1["rho"])
+    d1["bot"] = _calc_bot(d1["top"][0], a, D, dD, L,  d1["rho"])
     
-    val_idx = d2["top"]>=d2["bot"]
+    val_idx = d1["top"]>=d1["bot"]
     
-    for key, arr in d2.items():
-        d2[key] = arr[val_idx]
+    for key, arr in d1.items():
+        d1[key] = arr[val_idx]
 
-    return(d2)
+    return(d1)
 
-def create_top_bot(phi, d2):
-    d3 = {}
+def create_top_bot(phi, d1):
+    d2 = {}
 
     phis = np.linspace(-phi/2, phi/2, num=ncell-20)
-    d3["x"], d3["y"] = _pol2cart(*np.meshgrid(d2["rho"], phis))
+    d2["x"], d2["y"] = _pol2cart(*np.meshgrid(d1["rho"], phis))
     
     ##Fit ellipse for bottom
-    d3["bots"]= _ellipse(phi/2, d2["bot"], phis).T
+    d2["bots"]= _ellipse(phi/2, d1["bot"], phis).T
     
     ##Create tops
-    d3["tops"], _ = np.meshgrid(d2["top"], phis)
+    d2["tops"], _ = np.meshgrid(d1["top"], phis)
     
     ##Clip off weird part where tops and bot intersect
-    d3["tops"] = _clip_f(d3["tops"], d2["slope"], a_min = d3["bots"])
-    d3["bots"] = _clip_f(d3["bots"], ~d2["slope"], a_max = d3["tops"])
-    nan_idx = np.isclose(d3["tops"], d3["bots"])
-    nan_idx[:, ~d2["slope"]] = False
-    for i, arr in d3.items():
+    d2["tops"] = _clip_f(d2["tops"], d1["slope"], a_min = d2["bots"])
+    d2["bots"] = _clip_f(d2["bots"], ~d1["slope"], a_max = d2["tops"])
+    nan_idx = np.isclose(d2["tops"], d2["bots"])
+    nan_idx[:, ~d1["slope"]] = False
+    for i, arr in d2.items():
         arr[nan_idx]=np.nan
-    return(d3, nan_idx, phis)
+    return(d2, nan_idx, phis)
 
-def create_confining_layer(clay_conf, d3, d2, phis, L, a, frac):
-    d3_clay = {}
-    idx_onshore = d2["rho"] < L*a
-    x_ons, y_ons = _pol2cart(*np.meshgrid(d2["rho"][idx_onshore], phis))
+def create_confining_layer(clay_conf, d2, d1, phis, L, a, frac):
+    d2_clay = {}
+    idx_onshore = d1["rho"] < L*a
+    x_ons, y_ons = _pol2cart(*np.meshgrid(d1["rho"][idx_onshore], phis))
     
     x_offset=L * a * (1-clay_conf)
     rho_clay, phi_clay = _cart2pol(x_ons[0][-1] - x_offset, y_ons[0][-1])
     rhos_clay = np.linspace(0, rho_clay, num=ncell)
     phis_clay = np.linspace(-phi_clay, phi_clay, num=ncell-20)
     
-    topf = interpolate.interp1d(d2["rho"], d2["top"])
-    botf = interpolate.interp1d(d2["rho"], d2["bot"])
+    topf = interpolate.interp1d(d1["rho"], d1["top"])
+    botf = interpolate.interp1d(d1["rho"], d1["bot"])
     
     depth_conf = topf(rhos_clay+x_offset) + botf(rhos_clay+x_offset) * frac
-    d3_clay["bots"] = -_ellipse(phi_clay, depth_conf, phis_clay).T
-    d3_clay["tops"] = np.zeros(d3_clay["bots"].shape) + topf(rhos_clay+x_offset) #Hack to get 2D tops of clay layer
+    d2_clay["bots"] = -_ellipse(phi_clay, depth_conf, phis_clay).T
+    d2_clay["tops"] = np.zeros(d2_clay["bots"].shape) + topf(rhos_clay+x_offset) #Hack to get 2D tops of clay layer
     
-    d3_clay["x"], d3_clay["y"] = _pol2cart(*np.meshgrid(rhos_clay, phis_clay))
-    d3_clay["x"] += x_offset
+    d2_clay["x"], d2_clay["y"] = _pol2cart(*np.meshgrid(rhos_clay, phis_clay))
+    d2_clay["x"] += x_offset
     
     #deactivate clay_layer past coastline
-    nan_clay = np.greater(d3_clay["x"].T, np.nanmax(x_ons, axis = 1)).T
-    for key, arr in d3_clay.items():
+    nan_clay = np.greater(d2_clay["x"].T, np.nanmax(x_ons, axis = 1)).T
+    for key, arr in d2_clay.items():
         arr[nan_clay] = np.nan
-#        d3_clay[key] = arr[~nan_clay]
-    return(d3_clay, nan_clay)
+#        d2_clay[key] = arr[~nan_clay]
+    return(d2_clay, nan_clay)
 
 def get_targgrid(dx, dy, L, phi_max):
     x_edge, y_edge = _pol2cart(L, phi_max)
@@ -137,12 +137,12 @@ def get_targgrid(dx, dy, L, phi_max):
     y_out = np.arange(-y_max, y_max+1, dy)
     return(np.meshgrid(x_out, y_out))
 
-def create_clayer(frac, bot, d3, phis, phi, a_off = 0.):
+def create_clayer(frac, bot, d2, phis, phi, a_off = 0.):
     
-#    depth_clay = d2["top"] + (d2["top"]+d2["bot"])*(frac)
+#    depth_clay = d1["top"] + (d1["top"]+d1["bot"])*(frac)
     depth_clay = a_off + bot*frac
     clayer = (_ellipse(phi/2, depth_clay, phis).T + depth_clay)/2.
-    in_prism=(clayer<d3["tops"]) & (clayer>d3["bots"])
+    in_prism=(clayer<d2["tops"]) & (clayer>d2["bots"])
     clayer[~in_prism] = np.nan
     return(clayer)
 
@@ -159,19 +159,19 @@ def pol2griddata(poldata, nan_idx, griddata, key_prep = ""):
     return(griddata)
 
 #%%Plot functions
-def clayer_plot(d3, d3_conf, figfol):
+def clayer_plot(d2, d2_conf, figfol):
     slcs = [np.s_[:, 50],np.s_[90, :]]
     dims = ["y", "x"]
     fig, axes = plt.subplots(ncols=2, figsize=(8, 4))
     
     for i, slc in enumerate(slcs):
-        xtop = xbot = d3[dims[i]][slc]
-        axes[i].plot(xbot, d3["bots"][slc], label = "bot")
-        axes[i].plot(xtop, d3["tops"][slc], label = "top")
+        xtop = xbot = d2[dims[i]][slc]
+        axes[i].plot(xbot, d2["bots"][slc], label = "bot")
+        axes[i].plot(xtop, d2["tops"][slc], label = "top")
         
         if dims[i] == "y":
-            xval = d3["x"][slc][90]
-            idx = np.searchsorted(d3_conf["x"][90, :], xval)
+            xval = d2["x"][slc][90]
+            idx = np.searchsorted(d2_conf["x"][90, :], xval)
             idx = np.s_[:, idx]
         else:
             axes[i].axvline(x=(a+b)*L, ls=":", color=".20")
@@ -179,14 +179,14 @@ def clayer_plot(d3, d3_conf, figfol):
             axes[i].axvline(x=L, color="k")
             idx = slc
             
-        xconf = d3_conf[dims[i]][idx]
+        xconf = d2_conf[dims[i]][idx]
     
-        axes[i].fill(np.append(xconf[::-1], xconf), np.append(d3_conf["tops"][idx][::-1], d3_conf["bots"][idx]), label="conf")
+        axes[i].fill(np.append(xconf[::-1], xconf), np.append(d2_conf["tops"][idx][::-1], d2_conf["bots"][idx]), label="conf")
         
         for j in range(n_clay):
-            xtop = xbot = d3[dims[i]][slc]
-            top = d3["ct%d"%j][slc]
-            bot = d3["cb%d"%j][slc]
+            xtop = xbot = d2[dims[i]][slc]
+            top = d2["ct%d"%j][slc]
+            bot = d2["cb%d"%j][slc]
             
             xtop = xtop[~np.isnan(top)]
             xbot = xbot[~np.isnan(bot)]
@@ -246,16 +246,16 @@ clay_conf = clay_conf[2]
 n_clay = n_clay[3]
 
 #%%Process central crosssection
-d2 = create_crosssection(a, alpha, b, beta, gamma, dD, D, L)
+d1 = create_crosssection(a, alpha, b, beta, gamma, dD, D, L)
 
 #%%Create 3D top & bottom
-d3, nan_idx, phis = create_top_bot(phi, d2)
+d2, nan_idx, phis = create_top_bot(phi, d1)
 
 ##Plot
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.plot_surface(d3["x"], d3["y"], d3["bots"], zorder=1)
-ax.plot_surface(d3["x"], d3["y"], d3["tops"], zorder=20)
+ax.plot_surface(d2["x"], d2["y"], d2["bots"], zorder=1)
+ax.plot_surface(d2["x"], d2["y"], d2["tops"], zorder=20)
 plt.savefig(os.path.join(figfol, "3D_top_bot.png"))
 plt.close()
 
@@ -263,46 +263,46 @@ plt.close()
 frac_sand = (1-SM) / (n_clay + 1)
 frac_clay = SM/(n_clay+1)
 
-d3_conf,nan_conf = create_confining_layer(clay_conf, d3, d2, phis, L, a, frac_clay)
+d2_conf,nan_conf = create_confining_layer(clay_conf, d2, d1, phis, L, a, frac_clay)
 
 #%%Create clay layers
 fracs = np.cumsum([frac_sand] + [frac_clay, frac_sand]*n_clay)
 
 for i in range(n_clay):
-    d3["ct%d"%i] = create_clayer(fracs[0::2][i], d2["bot"], d3, phis, phi)
-    d3["cb%d"%i] = create_clayer(fracs[1::2][i], d2["bot"], d3, phis, phi)
+    d2["ct%d"%i] = create_clayer(fracs[0::2][i], d1["bot"], d2, phis, phi)
+    d2["cb%d"%i] = create_clayer(fracs[1::2][i], d1["bot"], d2, phis, phi)
 
 ##Plot
-clayer_plot(d3, d3_conf, figfol)
+clayer_plot(d2, d2_conf, figfol)
 
 #%%Convert to Cartesian regular grid
-d3_grid = {}
+d2_grid = {}
 
 dx, dy = 1000, 1000
 
-d3_grid["x"], d3_grid["y"] = get_targgrid(dx, dy, L, phi/2)
-d3_grid = pol2griddata(d3, nan_idx, d3_grid)
-d3_grid = pol2griddata(d3_conf, nan_conf, d3_grid, "conf_")
+d2_grid["x"], d2_grid["y"] = get_targgrid(dx, dy, L, phi/2)
+d2_grid = pol2griddata(d2, nan_idx, d2_grid)
+d2_grid = pol2griddata(d2_conf, nan_conf, d2_grid, "conf_")
 
-plt.imshow(d3_grid["tops"])
+plt.imshow(d2_grid["tops"])
 plt.savefig(os.path.join(figfol, "top_grid.png"))
 plt.close()
 
-plt.imshow(d3_grid["bots"])
+plt.imshow(d2_grid["bots"])
 plt.savefig(os.path.join(figfol, "bot_grid.png"))
 plt.close()
 
 #%%Create topsystem
-d3_grid["topsys"] = (np.nan_to_num(d3_grid["tops"]/d3_grid["tops"]) + 
-           np.nan_to_num(d3_grid["conf_tops"]/d3_grid["conf_tops"]) + 
-                                                  (d3_grid["tops"] >= 0)).astype(np.int16)
+d2_grid["topsys"] = (np.nan_to_num(d2_grid["tops"]/d2_grid["tops"]) + 
+           np.nan_to_num(d2_grid["conf_tops"]/d2_grid["conf_tops"]) + 
+                                                  (d2_grid["tops"] >= 0)).astype(np.int16)
 
-plt.imshow(d3_grid["topsys"])
+plt.imshow(d2_grid["topsys"])
 plt.savefig(os.path.join(figfol, "topsystem_grid.png"))
 plt.close()
 
 #%%Create 3D 
-d3_grid = xr.Dataset(dict([(key, (["y", "x"], value)) for key, value in d3_grid.items() if key not in ["x", "y"]]),
-                     coords = {"x" : d3_grid["x"][0, :],
-                               "y" : d3_grid["y"][:, 0]})
+d2_grid = xr.Dataset(dict([(key, (["y", "x"], value)) for key, value in d2_grid.items() if key not in ["x", "y"]]),
+                     coords = {"x" : d2_grid["x"][0, :],
+                               "y" : d2_grid["y"][:, 0]})
 
