86126af82e185c84877c09266301a4a21dba01a6
Joeri van Engelen <Joeri.vanEngelen@deltares.nl>
2019-05-07 14:29:52 +0200

    start on scripts that can be called every 9999 years to glue together different models

---
+++ delta_aquifer/process/idf2nc.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  7 11:24:25 2019
+
+@author: engelen
+"""
+
+import imod
+import xarray as xr
+import sys, os
+
+#modelfol = sys.arg[1]
+modelfol = r"g:\synthdelta\test_idf_output2"
+
+#s_nr = sys.argv[2]
+s_nr = 1
+
+#mod_nr = sys.argv[3]
+modnr = 1
+
+path = os.path.join(modelfol, "results" ,"*", "*_p{:03d}.IDF".format(s_nr))
+
+ds = imod.idf.open_dataset(path, use_cftime=True, pattern=r"{name}_{time}_l{layer}_p\d*")
+nc_path = os.path.join(modelfol, "results", "results_{:03d}_p{:03d}.nc".format(modnr, s_nr))
+xr.merge(ds.values()).to_netcdf(nc_path)
