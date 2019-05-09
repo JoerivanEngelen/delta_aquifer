509e1b50c0bb88154e9f9676b192ab19f06e3a6b
Joeri van Engelen <Joeri.vanEngelen@deltares.nl>
2019-05-09 16:50:54 +0200

    Made sure encoding is done correctly, this gave errors otherwise

--- process/idf2nc.py
+++ process/idf2nc.py
@@ -13,9 +13,9 @@ modelfol = sys.argv[1]
 mod_nr = int(sys.argv[2])
 s_nr = int(sys.argv[3])
 ##For testing
-#modelfol = r"g:\synthdelta\test_idf_output"
+#modelfol = r"g:\synthdelta\test_idf_output2"
 #mod_nr = 1
-#s_nr = 17
+#s_nr = 0
 
 #%%Path management
 path = os.path.join(modelfol, "results" ,"*", "*_p{:03d}.IDF".format(s_nr))
@@ -23,4 +23,7 @@ path = os.path.join(modelfol, "results" ,"*", "*_p{:03d}.IDF".format(s_nr))
 #%%Process
 ds = imod.idf.open_dataset(path, use_cftime=True, pattern=r"{name}_{time}_l{layer}_p\d*")
 nc_path = os.path.join(modelfol, "results", "results_{:03d}_p{:03d}.nc".format(mod_nr, s_nr))
-xr.merge(ds.values()).to_netcdf(nc_path)
+ds = xr.merge(ds.values())
+ds.time.encoding["units"] = r"days since 1999-01-01"
+ds.time.encoding["calendar"] = r"proleptic_gregorian"
+ds.to_netcdf(nc_path, unlimited_dims=["time"])
