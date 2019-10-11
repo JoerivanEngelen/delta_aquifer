6f60c8e05e69e364e15ba063e10860c398562a5f
Joeri van Engelen <joerivanengelen@hotmail.com>
2019-10-11 14:02:25 +0200

    Made amount of variables to be plotted more generic

--- analyze_input/parameter_ranges.py
+++ analyze_input/parameter_ranges.py
@@ -47,8 +47,10 @@ df['Kaqt_max'] = df['Kaqt_max'].where(to_keep, df['Kaqf_max']/df['Anisotropy_min
 df['Anisotropy_min'] = df['Anisotropy_min'].where(to_keep, np.nan)
 df['Anisotropy_max'] = df['Anisotropy_max'].where(to_keep, np.nan)
 
-hdrglgy = ["Kaqf", "Kaqt", "Recharge", "Anisotropy"]
+hdrglgy = ["Kaqf", "Kaqt", "Recharge", "Anisotropy", "Ss"]
 df_hdrglgy_ls = [get_df_plot(df, var, i) for i, var in enumerate(hdrglgy)]
+#TODO: 'Delta' has duplicate values, so marge creates huge Dataframe with lots of NaNs
+#This does not influence the figures, but is confusing to the naive user.
 df_hdrglgy = reduce(lambda x, y: pd.merge(x, y, on = 'Delta'), df_hdrglgy_ls)
 
 sheets = ["BC_Raw", "Lithology_Raw", "Geometry_Raw"]
@@ -60,47 +62,18 @@ df_all = reduce(lambda x, y: pd.merge(x, y, on = 'Delta'), df_ls)
 df_all['l_a'].where((df_all['L_b'] < 500.) , other=np.nan, inplace=True)
 df_all['l_a'].where((df_all['Delta'] != "Nakdong"), other=np.nan, inplace=True)
 
-#%%Plot hydrogeology ranges per delta and histograms for the rest
-#Set defaults
-sns.set(style="darkgrid")
-sns.set_context("paper")
+#%%Select variables to plot
 
-#Create figure and gridspec
-fig = plt.figure(figsize = agu_whole)
+#Ranges of hydrogeological parameters
+hdrglgy2plot = OrderedDict((
+            ("logKaqf",  "$\log(K_{h,aqf} \; [m/d])$"),
+            ("logKaqt",  "$\log(K_{v,aqt} \; [m/d])$"), 
+            ("Recharge", "$R \; [m/d]$"), 
+            ("logAnisotropy", "$\log(K_h/K_v \; [-])$"),
+            ("logSs", "$\log(S_s)$")
+            ))
 
-gs0 = gridspec.GridSpec(2, 1, figure=fig)
-gs00 = gridspec.GridSpecFromSubplotSpec(1, 5, subplot_spec=gs0[0])
-gs01 = gridspec.GridSpecFromSubplotSpec(2, 5, subplot_spec=gs0[1], hspace=0.3, wspace=0.3)
-
-hdrglgy_ax_lst = [fig.add_subplot(gs00[1])]
-hdrglgy_ax_lst += [fig.add_subplot(gs00[i], sharey=hdrglgy_ax_lst[0]) for i in range(2,5)]
-
-hist_ax_lst = [fig.add_subplot(gs01[i]) for i in range(10)]
-
-##Plot input ranges for hydrogeology
-opts = dict(ci="range", join=False, errwidth=1.5,scale=0.4)
-
-var2plot = OrderedDict(
-            logKaqf = "$\log(K_{h,aqf} \; [m/d])$",
-            logKaqt = "$\log(K_{v,aqt} \; [m/d])$", 
-            Recharge = "$R \; [m/d]$", 
-            logAnisotropy = "$\log(K_h/K_v \; [-])$"
-            )
-
-for i, var in enumerate(var2plot.keys()):
-    pointplotrange(y="Delta", x=var, data=df_hdrglgy, ax=hdrglgy_ax_lst[i], hue="n_pubs_%s"%i, **opts)
-    labels = dict(xlabel=var2plot[var])
-    if i != 0:
-        labels["ylabel"] = ""
-    hdrglgy_ax_lst[i].set(**labels)
-
-for i in [0, 1, 2]:
-    #Get rid of automatically added legends
-    hdrglgy_ax_lst[i].get_legend().remove()
-    #We have to remove ticklabels manually, as gridspec does not do hide this
-    plt.setp(hdrglgy_ax_lst[i+1].get_yticklabels(), visible=False)
-
-##Plot histograms for the other parameters
+#Histograms for other parameters
 var2plot = OrderedDict((("l_a",         "$l_a$"), 
                         ("beta",        r"$\beta$"),
                         (r"H_a/H_b",    "$f_H$"),
@@ -113,6 +86,44 @@ var2plot = OrderedDict((("l_a",         "$l_a$"),
                         ('N_chan',      "$N_{chan}$")
                         ))
 
+#%%Plot hydrogeology ranges per delta and histograms for the rest
+#Set defaults
+sns.set(style="darkgrid")
+sns.set_context("paper")
+
+n_range = len(hdrglgy2plot.keys())
+n_hist = len(var2plot.keys())
+n_hist_cols = np.ceil(n_hist/2).astype(int)
+
+#Create figure and gridspec
+fig = plt.figure(figsize = agu_whole)
+
+gs0 = gridspec.GridSpec(2, 1, figure=fig)
+gs00 = gridspec.GridSpecFromSubplotSpec(1, n_range+1, subplot_spec=gs0[0])
+gs01 = gridspec.GridSpecFromSubplotSpec(2, n_hist_cols, subplot_spec=gs0[1], hspace=0.3, wspace=0.3)
+
+hdrglgy_ax_lst = [fig.add_subplot(gs00[1])]
+hdrglgy_ax_lst += [fig.add_subplot(gs00[i], sharey=hdrglgy_ax_lst[0]) for i in range(2,n_range+1)]
+
+hist_ax_lst = [fig.add_subplot(gs01[i]) for i in range(n_hist)]
+
+##Plot input ranges for hydrogeology
+opts = dict(ci="range", join=False, errwidth=1.5,scale=0.4)
+
+for i, var in enumerate(hdrglgy2plot.keys()):
+    pointplotrange(y="Delta", x=var, data=df_hdrglgy, ax=hdrglgy_ax_lst[i], hue="n_pubs_%s"%i, **opts)
+    labels = dict(xlabel=hdrglgy2plot[var])
+    if i != 0:
+        labels["ylabel"] = ""
+    hdrglgy_ax_lst[i].set(**labels)
+
+for i in range(n_range-1):
+    #Get rid of automatically added legends
+    hdrglgy_ax_lst[i].get_legend().remove()
+    #We have to remove ticklabels manually, as gridspec does not do hide this
+    plt.setp(hdrglgy_ax_lst[i+1].get_yticklabels(), visible=False)
+
+##Plot histograms for the other parameters
 opts = dict(hist=True, rug=False, kde=False, bins='doane')
 
 for i, var in enumerate(var2plot.keys()):
