91fca8d62dcbb0439256a20eb73608ee9819bdef
Joeri van Engelen <joerivanengelen@hotmail.com>
2019-11-07 15:40:10 +0100

    Included ouputs of interest

--- post/fw_volumes.py
+++ post/fw_volumes.py
@@ -10,6 +10,7 @@ from glob import glob
 import xarray as xr
 import numpy as np
 from delta_aquifer import initial_conditions as ic
+import pandas as pd
 
 if os.name == "posix":
     import matplotlib 
@@ -21,6 +22,23 @@ else:
 
 import seaborn as sns
 
+def spat_sum(da, name):
+    return(da.sum(dim=["x", "y", "z"]).rename(name))
+
+def cftime_to_ka(ds):
+    years = np.array([t.year for t in ds.time.values])
+    years = (years - years[-1])/1000 * -1
+    
+    return(ds.assign_coords(time=years))
+
+def coord_of_max(da):
+    return(da.where(da==da.max(), drop=True).squeeze())
+
+#%%Ideas
+#Upward salt flux? -> Check if can even occur in this model
+#How much recharge goes in, how much discharged?
+#   -> qz*A/rch
+
 #%%Path management
 if len(sys.argv) > 1:
     fol  = sys.argv[1]
@@ -31,58 +49,88 @@ else:
 nc_paths = glob(os.path.join(fol, "results_*.nc"))
 nc_paths.sort()
 
+bc = os.path.join(fol, "input", "data", "bcs.nc")
+
 #%%pre-process data for analysis
+sea_level = xr.open_dataset(bc)["sea_level"]
+sea_level = cftime_to_ka(sea_level)
+
 ds = xr.open_mfdataset(nc_paths, 
                        chunks={"time" : 2, "z": -1, "y": -1, "x": -1},
                        combine="nested",
                        concat_dim="time")
 ds["vol"] = np.abs(ds.dx * ds.dy * ds.dz)
-
-years = np.array([t.year for t in ds.time.values])
-years = (years - years[-1])/1000 * -1
-
-ds = ds.assign_coords(time=years)
+ds = cftime_to_ka(ds)
 
 #%%Get coastline
 active = (ds["conc1"] > -1.)
-onshore = active.sel(z=0, method="nearest")
+#To have a moving coastline through time, I have not enough info
+#For this I probably have to store coastline_x in bcs.nc and read this
+#Right now not a problem because I'm looking at the endstate offshore/onshore stuff
+onshore = active.isel(time=-1).sel(z=0, method="nearest")
 x_loc=xr.where(onshore, onshore.x, np.nan).max(dim="x")
 x_loc=x_loc.fillna(x_loc.min()) #fillna
 
-#%%Calculate fw_vols
+#%%Calculate vols and masses
 fw_vols  = xr.where((ds["conc1"] > -1.) & (ds["conc1"] < 1.),  ds["vol"], 0)
-tot_vols = xr.where(active, ds["vol"], 0)
 fw_vols_offshore = xr.where((fw_vols!=0) & (ds.x > x_loc), fw_vols, 0)
 
-brsal_onshore=xr.where(
-        (ds["conc1"] > 0.) & (ds.x < x_loc), 
+tot_vols = xr.where(active, ds["vol"], 0)
+tot_masses = ic.c2dens(ds["conc1"]*active)*tot_vols
+sal_masses=xr.where(
+        (ds["conc1"] > 0.), 
         ds["conc1"]*ds["vol"], 0
-        ).sum(dim=["x", "y", "z"]
-        ).rename("sbw_onshore")
+        )
 
-fw_vol  = fw_vols.sum(dim=["x", "y", "z"]).rename("fw")
-fw_vol_offshore = fw_vols_offshore.sum(dim=["x", "y", "z"]).rename("fw_offshore")
-br_vol  = xr.where((ds["conc1"] > 1.)  & (ds["conc1"] < 30.), ds["vol"], 0).sum(dim=["x", "y", "z"]).rename("bw")
-sal_vol = xr.where(ds["conc1"] > 30., ds["vol"], 0).sum(dim=["x", "y", "z"]).rename("sw")
-old_vol = xr.where(ds["conc2"] > 0.5, ds["vol"], 0).sum(dim=["x", "y", "z"]).rename("ow")
+mas={}
+mas["sal"]         = spat_sum(sal_masses, "sal")
+mas["ol_sal"]      = spat_sum(sal_masses*ds["conc2"], "ol_sal")
+mas["sal_onshore"] = spat_sum(xr.where((ds.x < x_loc), sal_masses, 0), "sal_onshore")
+mas["tot"]         = spat_sum(tot_masses, "tot")
+mas["tot_onshore"] = spat_sum((ds.x < x_loc)*tot_masses, "tot_onshore")
+mas=xr.Dataset(data_vars=mas)
 
-tot_vol = tot_vols.sum(dim=["x", "y", "z"])
-tot_masses = (ic.c2dens(ds["conc1"]*active)*tot_vols)
-tot_mas = tot_masses.sum(dim=["x", "y", "z"])
-tot_mass_onshore = ((ds.x > x_loc)*tot_masses).sum(dim=["x", "y", "z"])
+vol={}
+vol["tot"]         = spat_sum(tot_vols,"tot")
+vol["fw"]          = spat_sum(fw_vols, "fw")
+vol["fw_offshore"] = spat_sum(fw_vols_offshore, "fw_offshore")
+vol["bw"]          = spat_sum(xr.where((ds["conc1"] > 1.)&(ds["conc1"] < 30.), 
+                               ds["vol"], 0), "bw")
+vol["sw"]          = spat_sum(xr.where(ds["conc1"] > 30., ds["vol"], 0), "sw")
+vol["ow"]          = spat_sum(xr.where(ds["conc2"] > 0.5, ds["vol"], 0), "ow")
+vol=xr.Dataset(data_vars=vol)
 
-frac_vols = xr.merge([fw_vol, br_vol, sal_vol, old_vol, fw_vol_offshore])/tot_vol
-frac_mas = xr.merge([brsal_onshore])/tot_mass_onshore
+frac_vols = vol/vol["tot"]
+frac_mas = mas[["sal", "ol_sal", "sal_onshore"]]
+frac_mas["sal"] = mas["sal"]/mas["tot"]
+frac_mas["ol_sal"] = mas["ol_sal"]/mas["sal"]
+frac_mas["sal_onshore"] = mas["sal_onshore"]/mas["tot_onshore"]
 
 frac_vols.to_netcdf(os.path.join(fol, "frac_vols.nc"))
+frac_mas.to_netcdf( os.path.join(fol, "frac_mas.nc"))
+
+#%%Read again to remove chunking
+frac_vols = xr.open_dataset(os.path.join(fol, "frac_vols.nc"))
+frac_mas  = xr.open_dataset(os.path.join(fol, "frac_mas.nc"))
+
+#%%Differentiate
+grad_fw = frac_vols["fw"].differentiate("time") * -1 #Multiply with -1 because the time axis is decreasing
+grad_sl = sea_level.differentiate("time") * -1 
 
 #%%Outputs of interest
-end_fw      = frac_vols["fw"].isel(time=-1).values
-max_change  = frac_vols["fw"].max().values - end_fw
-#old water should be old water mass normalized over total salt mass instead of volume ratio
-old_water   = (frac_vols["ow"]/frac_vols["sw"]).isel(time=-1).values
-onshore_sw  = frac_mas["sbw_onshore"].isel(time=-1).values
-offshore_fw = frac_vols["fw_offshore"].isel(time=-1).values
+oi = {}
+oi["end_fw"]          = frac_vols["fw"].isel(time=-1).values
+oi["offshore_fw"]     = frac_vols["fw_offshore"].isel(time=-1).values
+oi["max_fw_decrease"] = frac_vols["fw"].max().values - oi["end_fw"] 
+oi["old_water"]       = frac_mas["ol_sal"].isel(time=-1).values
+oi["onshore_sw"]      = frac_mas["sal_onshore"].isel(time=-1).values/(35./1025.)
+oi["fw_gradient"]     = (grad_fw).isel( 
+                            time=slice(-3, None)
+                            ).mean()
+oi["delay"] = coord_of_max(grad_sl).time - coord_of_max(grad_fw*-1).time 
+
+keys, values = list(zip(*oi.items()))
+oi = pd.DataFrame(data={"var" : keys, "value" : values}).set_index("var")
 
 #%%Plot
 sns.set_style("darkgrid", {"axes.facecolor": ".4", "xtick.bottom": True})
