b02f5b6fe45ef752cd177bba840769ee59ab2e93
Joeri van Engelen <joerivanengelen@hotmail.com>
2019-11-13 14:31:58 +0100

    Added script to write initial conditions from previous succesfully saved result data so one can continue a crashed modelfrom the last succesfully written results (in .nc)

---
+++ process/write_inits_continue.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 12 11:36:12 2019
+
+@author: engelen
+
+Script to generate input for a continuation run based on previous output
+
+"""
+
+import xarray as xr
+from glob import glob
+from imod import idf
+import sys, os
+import numpy as np
+import cftime
+
+#%%Path management
+if len(sys.argv) > 1:
+    res_folder  = sys.argv[1]
+    model_folder = sys.argv[2] 
+else:
+    #Local testing on my own windows laptop
+    res_folder = r"g:\synthdelta\results\test_output\synth_SD_i029_m24_7065039"
+    model_folder = r"c:\Users\engelen\test_imodpython\synth_delta_test\SD_i029"
+    
+files = glob(os.path.join(res_folder, "results_[0-9][0-9][0-9].nc"))
+files.sort()
+
+cont_nr = len(files)
+
+mname = os.path.basename(model_folder)
+sub_model_path = os.path.join(model_folder, mname+"_nr{:02d}".format(cont_nr))
+
+#%%Get appropriate time
+
+#Created in create_model.py. Update this if changed in create_model.py
+sub_ends=np.array([8000., 8000., 5000., 5000., 8000., 8000., 4000.]) 
+start_year = 1999
+
+time = cftime.DatetimeProlepticGregorian(
+        sub_ends[cont_nr-1]+start_year, 1, 1)
+
+#%%Process data
+#Load
+ds_last = xr.open_dataset(files[cont_nr-1])
+ds_ini = ds_last.isel(time=-1)[["conc1","conc2", "head"]].load()
+
+#Expand conc with species
+ds_ini = ds_ini.rename(name_dict={"conc1" : "conc"})
+ds_ini["conc"] = ds_ini["conc"].expand_dims(species=[1,2])
+ds_ini["conc"] = xr.where(ds_ini["conc"].species==2, 
+      ds_ini["conc2"], ds_ini["conc"])
+ds_ini = ds_ini.drop(labels="conc2")
+
+#Ensure appropriate time is assigned (the call to update_timesteps.py messed this up)
+ds_ini = ds_ini.assign_coords(time=time)
+
+#Save
+idf.save(os.path.join(sub_model_path, "bas", "head"), ds_ini["head"])
+idf.save(os.path.join(sub_model_path, "btn", "conc"), ds_ini["conc"], 
+         pattern = r"{name}_{time:%Y%m%d%H%M%S}_c{species}_l{layer}{extension}")
