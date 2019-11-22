baed3737ae9e9abeb143edbb9fd9ab7653c51d80
Joeri van Engelen <joerivanengelen@hotmail.com>
2019-11-22 13:29:09 +0100

    Added script to correct bcs times.

---
+++ delta_aquifer/correct_bcs_time.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 22 12:53:45 2019
+
+@author: engelen
+"""
+
+import sys, os
+import xarray as xr
+import numpy as np
+import cftime
+
+if len(sys.argv) > 1:
+    model_fol  = sys.argv[1]
+else:
+    #Local testing on my own windows laptop
+    model_fol = r"g:\synthdelta\results\test_output\synth_SD_i123_m24_7114121"
+    
+#%%Path management
+path = os.path.join(model_fol, "input", "data", "bcs.nc")
+
+#%%Load
+ds = xr.open_dataset(path)
+ds.load()
+ds.close()
+
+units = "days since 0000-01-01"
+calendar = '365_day'
+
+#%%Correct timesteps
+timedeltas = ds.time.values - ds.time[0].values
+years = np.round([timedelta.days/365 for timedelta in timedeltas], -1)
+years = cftime.num2date(np.array(years)*365, units=units, calendar=calendar)
+
+ds = ds.assign_coords(coords={"time" : years})
+ds["time"].encoding["units"] = units
+ds["time"].encoding["calendar"] = calendar
+
+#%%Save
+ds.to_netcdf(path)
