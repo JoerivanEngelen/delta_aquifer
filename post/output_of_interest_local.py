f18f25328d8422ad72bd610ed1deab945235c88c
Joeri van Engelen <joerivanengelen@hotmail.com>
2019-12-19 13:52:45 +0100

    Added some extra outputs of interest to look at

--- post/output_of_interest_local.py
+++ post/output_of_interest_local.py
@@ -37,15 +37,25 @@ for fracs_path in paths:
     
     #%%Differentiate
     grad_fw = fracs["fw"].differentiate("time") * -1 #Multiply with -1 because the time axis is decreasing
+    grad_s = fracs["m_sal_onshore"].differentiate("time") * -1
+    grad_fw_on = fracs["fw_onshore"].differentiate("time") * -1 
     
     #%%Outputs of interest
     oi = {}
     oi["end_fw"]          = fracs["fw"].isel(time=-1).values
+    oi["onshore_fw"]      = fracs["fw_onshore"].isel(time=-1).values
     oi["offshore_fw"]     = fracs["fw_offshore"].isel(time=-1).values
+    oi["onshore_bw"]      = fracs["bw_onshore"].isel(time=-1).values
     oi["max_fw_decrease"] = fracs["fw"].max().values - oi["end_fw"] 
     oi["old_water"]       = fracs["m_ol_sal"].isel(time=-1).values
     oi["onshore_sw"]      = fracs["m_sal_onshore"].isel(time=-1).values/(35./1025.)
     oi["fw_gradient"]     = (grad_fw).isel( 
+                                time=slice(-3, None)
+                                ).mean().values
+    oi["s_gradient"]      = (grad_s).isel( 
+                                time=slice(-3, None)
+                                ).mean().values
+    oi["fw_on_gradient"]  = (grad_fw_on).isel( 
                                 time=slice(-3, None)
                                 ).mean().values
     oi["delay"] = (t_start - coord_of_max(fracs["fw"]*-1).time ).values
