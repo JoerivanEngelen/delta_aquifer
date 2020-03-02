01168e553842fe8bd01dbc5d53575b9ec323796a
Joeri van Engelen <joerivanengelen@hotmail.com>
2020-03-02 15:46:59 +0100

    Code cleanup

--- create_inputs/depth_functions_abstraction.py
+++ create_inputs/depth_functions_abstraction.py
@@ -9,17 +9,42 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.stats as stats
 
 from glob import glob
 import os
 import imod
 
+def assign_bin_depths(df, depth_col, max_depth):
+    df["depth (m)"] = pd.cut(df[depth_col], np.arange(0, max_depth, 20), duplicates="drop")
+
+def sum_Q_per_depth(df, *Q_cols):
+    return(df.groupby("depth (m)").sum()[list(Q_cols)])
+
+def count_Q_per_depth(df, *Q_cols):
+    return(df.groupby("depth (m)").count()[list(Q_cols)])
+
+def bin_to_mids(index):
+    return([(a.left + a.right)/2 for a in index])
+
+def get_Q_per_depth(df, depth_col, max_depth, *Q_cols, method=sum_Q_per_depth):
+    assign_bin_depths(df, depth_col, max_depth)
+    
+    Q_sum_depth = sum_Q_per_depth(df, *Q_cols).sort_index(ascending=False)
+    
+    mid_depth = bin_to_mids(Q_sum_depth.index)
+
+    #Scale
+    Q_sum_depth /= Q_sum_depth.values.sum()
+    
+    #For some reason I had to reindex it twice befire the actual index was assigned
+    Q_sum_depth = Q_sum_depth.reindex(mid_depth)
+    Q_sum_depth = Q_sum_depth.reindex(mid_depth) 
+    return(Q_sum_depth)
 
 #%%Path management
 path_Nile = r"c:\Users\engelen\OneDrive - Stichting Deltares\PhD\Nile_Delta\Data\Well_Data\Extractions_All.xlsx"
 path_Mekong_folder = r"c:\Users\engelen\OneDrive - Stichting Deltares\PhD\Synth_Delta\Data\wells\Mekong\Voor_joeri\VERSION_1"
-path_out = r"c:\Users\engelen\OneDrive - Stichting Deltares\PhD\Synth_Delta\Data\depth_abstractions\Nile.png"
+path_out_Nile   = r"c:\Users\engelen\OneDrive - Stichting Deltares\PhD\Synth_Delta\Data\depth_abstractions\Nile.png"
 path_out_Mekong = r"c:\Users\engelen\OneDrive - Stichting Deltares\PhD\Synth_Delta\Data\depth_abstractions\Mekong.png"
 
 M_ipfs = glob(os.path.join(path_Mekong_folder, "WEL", "*.IPF"))
@@ -29,26 +54,16 @@ M_tops = glob(os.path.join(path_Mekong_folder, "TOP", "*.IDF"))
 #%%Read
 abs_Nile = pd.read_excel(path_Nile)
 
-#%%Process data Nile
+#%%Prepare data Nile
 abs_Nile["Screen_Mid"] = (abs_Nile["Screen_Top"] + abs_Nile["Screen_Bottom"])/2
 
 abs_Nile = abs_Nile[abs_Nile["Screen_Mid"] < 1000.]
 abs_Nile = abs_Nile.dropna(subset=["Irrigation", "Drinking", "Industrial", "Conjunctive_Use"])
 
-abs_Nile["depth (m)"] = pd.cut(abs_Nile["Screen_Mid"], np.arange(0, 321, 20), duplicates="drop")
+args_Nile = abs_Nile, "Screen_Mid", 321, "Irrigation", "Drinking", "Industrial", "Conjunctive_Use"
+Q_sum_depth_Nile = get_Q_per_depth(*args_Nile, method=sum_Q_per_depth,)
 
-Q_sum_depth = abs_Nile.groupby("depth (m)").sum()[["Irrigation", "Drinking", "Industrial", "Conjunctive_Use"]].reset_index()
-Q_sum_depth = Q_sum_depth.set_index("depth (m)").sort_index(ascending=False)
-
-mid_depth = [(a.left + a.right)/2 for a in Q_sum_depth.index]
-
-#Scale
-Q_sum_depth /= Q_sum_depth.values.sum()
-
-Q_sum_depth = Q_sum_depth.reindex(mid_depth)
-Q_sum_depth = Q_sum_depth.reindex(mid_depth)
-
-#%%Process data Mekong
+#%%Prepare data Mekong
 import xarray as xr
 
 lookup = {"QH" : 3,
@@ -68,6 +83,7 @@ abs_Mekong = abs_Mekong.drop(columns = ["Aquifer", "Year"])
 abs_Mekong["aquifer"] = abs_Mekong["name"].str.replace("2015", "")
 abs_Mekong["aquifer"] = abs_Mekong["aquifer"].str.replace("_HCMC", "")
 abs_Mekong["layer"] = abs_Mekong["aquifer"].replace(lookup)
+abs_Mekong["Q"] *= -1
 
 tops_Mekong = xr.concat([imod.idf.open(top) for top in M_tops], dim="layer").sortby("layer")
 bots_Mekong = xr.concat([imod.idf.open(bot) for bot in M_bots], dim="layer").sortby("layer")
@@ -80,35 +96,23 @@ z_select = z.sel(x=xr.DataArray(abs_Mekong["X"], coords = {"index" : abs_Mekong.
       y=xr.DataArray(abs_Mekong["Y"], dims="index"),
       layer=xr.DataArray(abs_Mekong["layer"], dims="index"), method="nearest")
 
-abs_Mekong["z"] = z_select.values
-abs_Mekong["depth (m)"] = pd.cut(abs_Mekong["z"]*-1, np.arange(0, 641, 20), duplicates="drop")
+abs_Mekong["z"] = z_select.values *-1
 
-Q_sum_depth_Mekong = abs_Mekong.groupby("depth (m)").sum()["Q"] * -1
-
-mid_depth_Mekong = [(a.left + a.right)/2 for a in Q_sum_depth_Mekong.index]
-
-Q_sum_depth_Mekong /= Q_sum_depth_Mekong.values.sum()
-
-Q_sum_depth_Mekong = Q_sum_depth_Mekong.reindex(mid_depth_Mekong)
-Q_sum_depth_Mekong = Q_sum_depth_Mekong.reindex(mid_depth_Mekong).sort_index(ascending=False)
-
-#%%Exponential distribution
-x = np.arange(0, 321, 20)
-scale = 1/0.30
-y = stats.expon.pdf(x/15, scale=scale)
+args_Mekong = abs_Mekong, "z", 641, "Q"
+Q_sum_depth_Mekong = get_Q_per_depth(*args_Mekong, method=sum_Q_per_depth)
 
 #%%Plot
 sns.set()
-ax = Q_sum_depth.plot(kind="barh", stacked=True)
-plt.plot(y, np.arange(len(x))[::-1]-1, color="k")
+ax = Q_sum_depth_Nile.plot(kind="barh", stacked=True)
 ax.set_xlabel("rel GW abstracted (-)")
 
 plt.tight_layout()
-plt.savefig(path_out, dpi=300)
+plt.savefig(path_out_Nile, dpi=300)
+plt.close()
 
-#ax = Q_sum_depth_Mekong.plot(kind="barh", stacked=True)
-#ax.set_xlabel("rel GW abstracted (-)")
-#
-#plt.tight_layout()
-#plt.savefig(path_out_Mekong, dpi=300)
-#%%Save
+ax = Q_sum_depth_Mekong.plot(kind="barh", stacked=True)
+ax.set_xlabel("rel GW abstracted (-)")
+
+plt.tight_layout()
+plt.savefig(path_out_Mekong, dpi=300)
+plt.close()
